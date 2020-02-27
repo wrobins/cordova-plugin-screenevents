1eaed8b33f0a24301dccdf2ff1ff6f52baf0faba
wrobins <wrobins@kent.edu>
2020-02-27 15:12:18 -0500

    Added Android logic.

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Cordova Screen Events Plugin
 Trigger events based on your device's screen state!
+## What is it?
+This plugin lets you set up a simple event listener that lets you respond to your user turning their phone's screen on or off. This lets you differentiate between your app simple going back and forth between background and foreground (pause and resume). These events only fire if the actual screen is being turned on or off.
 ## How do I install it?
-Coming Soon!
+<pre>
+cordova plugin add cordova-plugin-screenevents
+</pre>
 ## How do I use it?
-Coming Soon!
+Cordova plugin calls expect a single callback event, so I suggest you set up a listener similar to the code below:
+```js
+window.cordova.plugins.ScreenEvents.listenerInit(function(msg) {
+    // Handle success event here
+    window.cordova.plugins.ScreenEvents.listenerInit(function(msg) {
+        // Handle success event again
+        }, function(msg) {
+        // Handle fail event here
+        });
+    }, function(msg) {
+    // Handle fail event here
+});
+```
+You probably want "Success event" and "fail event" logic to be function calls to avoid duplicate code since you'll have to reference each twice.
+
+The msg variable will be a string, and one of three things: in a success scenario, it will be either "SCREEN_TURNED_ON" or "SCREEN_TURNED_OFF" which should be self-explanatory. Otherwise, in a failure scenario it will be whatever exception message your device's native platform decides to conjure.
---
+++ src/android/ScreenEvents.java
@@ -0,0 +1,70 @@
+package com.wrobins.cordova.plugin;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class ScreenEvents extends CordovaPlugin {
+    private Activity activity;
+    private Context context;
+    private CallbackContext callbackContext;
+
+    private static final String LISTENER_INIT = "listenerInit";
+    private static final String SCREEN_TURNED_ON = "SCREEN_TURNED_ON";
+    private static final String SCREEN_TURNED_OFF = "SCREEN_TURNED_OFF";
+
+    private class ScreenStateReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (Intent.ACTION_SCREEN_ON.equals(action)) {
+                ScreenEvents.this.callbackContext.success(SCREEN_TURNED_ON);
+            } else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+                ScreenEvents.this.callbackContext.success(SCREEN_TURNED_OFF);
+            }
+        }
+    }
+
+    private ScreenStateReceiver mReceiver;
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+
+        activity = cordova.getActivity();
+        context = webView.getContext();
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        this.callbackContext = callbackContext;
+
+        try {
+            if (LISTENER_INIT.equals(action)) {
+                this.listenerInit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            callbackContext.error(e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    private void listenerInit() {
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        mReceiver = new ScreenStateReceiver();
+        activity.registerReceiver(mReceiver, intentFilter);
+    }
+}
